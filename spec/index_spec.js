b923fc5d60956f840ccb1f80c4731768d61023c5
Themba-Dombo <themba.dombo@gmail.com>
2020-08-28 15:15:29 +0200

    updated test specs

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,40 +1,55 @@
-const { newVisitor, listAll,deleteVisitor,updateVisitor,viewVisitor,deleteAll } = require("../src/index");
+const { newVisitor, listAll, deleteVisitor, updateVisitor, viewVisitor, deleteAll } = require("../src/index");
 
-describe("newVisitor",()=>{
-    it("havebeen called",()=>{
-        const visitor = {newVisitor}
-        spyOn(visitor, "newVisitor")
-        let visitorSpy = visitor.newVisitor("Luke",21,"2020/06/01","11:24","Yvonne","for job intervie")
-        expect(visitor.newVisitor).toHaveBeenCalledWith("Luke",21,"2020/06/01","11:24","Yvonne","for job interview");
+describe("newVisitor", () => {
+    it("should have been called with name, age, dateOfVisit, timeOfVisit, assited by and comment", () => {
+        const visitor = { newVisitor }
+        spyOn(visitor, "newVisitor");
+        visitor.newVisitor("Luke",21,"2020/06/01","11:24","Yvonne","for job interview");
+        expect(visitor.newVisitor).toHaveBeenCalledWith("Luke", 21, "2020/06/01", "11:24", "Yvonne", "for job interview");
     });
 });
 
-describe("listAll",()=>{
-    it("havebeen called",()=>{
-        expect(listAll()).toHaveBeenCalled();
+describe("listAll", () => {
+    it("should have been called", () => {
+        const showAll = { listAll };
+        spyOn(showAll, "listAll");
+        showAll.listAll()
+        expect(showAll.listAll).toHaveBeenCalled();
     });
 });
 
-describe("deleteVisitor",()=>{
-    it("havebeen called",()=>{
-        expect(deleteVisitor()).toHaveBeenCalled();
+describe("deleteVisitor", () => {
+    it("should have been called with id and name", () => {
+        const deleted = { deleteVisitor };
+        spyOn(deleted, "deleteVisitor");
+        deleted.deleteVisitor(1, "Themba");
+        expect(deleted.deleteVisitor).toHaveBeenCalledWith(1,"Themba");
     });
 });
 
-describe("updateVisitor",()=>{
-    it("havebeen called",()=>{
-        expect(updateVisitor()).toHaveBeenCalled();
+describe("updateVisitor", () => {
+    it("should have been called with column name, new value and id", () => {
+        const updated = { updateVisitor };
+        spyOn(updated, "updateVisitor");
+        updated.updateVisitor("name","Themba",1)
+        expect(updated.updateVisitor).toHaveBeenCalledWith("name","Themba",1);
     });
 });
 
-describe("viewVisitor",()=>{
-    it("havebeen called",()=>{
-        expect(viewVisitor()).toHaveBeenCalled();
+describe("viewVisitor", () => {
+    it("should have been called with id", () => {
+        const view = { viewVisitor };
+        spyOn(view, "viewVisitor");
+        view.viewVisitor(1);
+        expect(view.viewVisitor).toHaveBeenCalledWith(1);
     });
 });
 
-describe("deleteAll",()=>{
-    it("havebeen called",()=>{
-        expect(deleteAll()).toHaveBeenCalled();
+describe("deleteAll", () => {
+    it("havebeen called", () => {
+        const deletedAll = { deleteAll };
+        spyOn(deletedAll, "deleteAll")
+        deletedAll.deleteAll();
+        expect(deletedAll.deleteAll).toHaveBeenCalled();
     });
 });
--- src/index.js
+++ src/index.js
@@ -1,25 +1,71 @@
-function newVisitor(){
-        return "visitor"
-    }
+const Pool = require("pg").Pool;
+const pool = new Pool({
+    user: "user",
+    host: "localhost",
+    database: "db",
+    password: "pass",
+    port: 5432
+});
 
-function listAll(){
-        return "all"
-    }
+function newVisitor(name, age, date, time, assistedby, comment,pol=pool) {
+    const sqlcommand = 'INSERT INTO visitors ("name", "age", "date_of_visit", "time_of_visit", "assited_by", "comments") VALUES($1, $2, $3, $4, $5, $6) RETURNING *'
+    const sqlvalues = [name, age, date, time, assistedby, comment];
+    pol.query(sqlcommand, sqlvalues, (err, res) => {
+        if (err) throw err;
+        console.log(res.rows);
+    })
+}
 
-function deleteVisitor(){
-        return "deleted"
-    }
+function listAll() {
+    pool.query("SELECT id, name FROM visitors", (err, res) => {
+        if (err) { throw err };
+        console.log(res.rows);
+    });
+}
 
-function updateVisitor(){
-        return "updated"
-    }
+function deleteVisitor(id, name) {
+    const sqlcommand = `DELETE FROM visitors WHERE ctid = (SELECT ctid FROM visitors WHERE id in ($1) AND name in ($2))`;
+    const sqlvalues = [id, name]
+    pool.query(sqlcommand, sqlvalues, (err, res) => {
+        if (err) { throw err };
+        console.log(res.sqlcommand);
+    })
+}
 
-function viewVisitor(){
-        return "vistor"
-    }
+function updateVisitor(column, newValue, id) {
+    if (column == "name" || column == "age" || column == "date_of_visit" || column == "time_of_visit" || column == "assited_by" || column == "comments") {
+        const sqlcommand = `UPDATE visitors SET ${column}=($1) WHERE id=($2)`;
+        const sqlvalues = [newValue, id]
+        pool.query(sqlcommand, sqlvalues, (err, res) => {
+            if (err) { throw err };
+            console.log(res.sqlcommand);
+        })
+    } else { console.log("incorrect column name") }
+}
 
-function deleteAll(){
-        return "all deleted"
-    }
+function viewVisitor(id) {
+    const sqlcommand = `SELECT name,age,date_of_visit,time_of_visit,assited_by,comments FROM visitors WHERE id=($1)`;
+    const value = [id]
+    pool.query(sqlcommand, value, (err, res) => {
+        if (err) { throw err };
+        console.log(res.rows);
+    });
+};
 
-module.exports = { newVisitor, listAll,deleteVisitor,updateVisitor,viewVisitor,deleteAll } 
+function deleteAll() {
+    const sqlcommand = `DELETE FROM visitors`;
+    pool.query(sqlcommand, (err, res) => {
+        if (err) { throw err };
+        console.log(res.sqlcommand);
+        console.log(res.rowCount);
+    });
+};
+
+//newVisitor("Luke",21,"2020/06/01","12:15","Ceasar","for interview");
+//updateVisitor('comments','do my laundry and wash my hair','8')
+//viewVisitor(8)
+//listAll()
+//deleteAll();
+
+
+module.exports = { newVisitor, listAll, deleteVisitor, updateVisitor, viewVisitor, deleteAll };
